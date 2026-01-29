7a13084efca8dc80643ec9916a5b08908e02fd9a
Nazar <famina1998@gmail.com>
2026-01-29 19:21:40 +0200

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Nazar on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
